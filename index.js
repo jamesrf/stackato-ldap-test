c05445e46b02347dfa31f8583c233ea609786bd4
James Fournie <jamesf@activestate.com>
2014-02-07 10:39:19 -0800

    Adding groups support and empty README

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 var ldap = require('ldapjs');
 
+var ldap_port = parseInt(process.env.STACKATO_HARBOR_LDAP_PORT) || 1389;
+var ldap_host = '0.0.0.0';
+
 var server = ldap.createServer();
 
 var SUFFIX = 'dc=example, dc=com';
 
 var userdb = {
   'stackato' : { samaccountname: 'stackato', email: 'stackato@stackato.com', password: 'stackato' },
-  'testuser' : { samaccountname: 'testuser', email: 'testuser@stackato.com', password: 'stackato' },
+  'testuser1' : { samaccountname: 'testuser1', email: 'testuser1@stackato.com', password: 'stackato' },
+  'testuser2' : { samaccountname: 'testuser2', email: 'testuser2@stackato.com', password: 'stackato' },
+  'testuser3' : { samaccountname: 'testuser3', email: 'testuser3@stackato.com', password: 'stackato' },
 };
+
+var groupdb = [
+  {cn:'stackato-admin', objectclass: 'posixgroup', memberuid:'stackato' },
+  {cn:'stackato-user', objectclass: 'posixgroup', memberuid:'testuser1' },
+  {cn:'stackato-user', objectclass: 'posixgroup', memberuid:'testuser2' },
+  {cn:'some-other-group', objectclass: 'posixgroup', memberuid:'testuser2' },
+  {cn:'some-other-group', objectclass: 'posixgroup', memberuid:'testuser3' }
+];
 
 var user = {};
 
 server.search(SUFFIX, function(req, res, next) {
-
   user.dn = req.dn.toString();
   
+  // check if this is a user query
   Object.keys(userdb).forEach( function(uid){
     if (req.filter.matches(userdb[uid])){
       user.attributes = userdb[uid];
       console.log('found user ' + uid);
       res.send(user);
+      res.end();
+      return next();
+    }
+  });
+
+  // check if this is a group query
+  groupdb.forEach( function(group) {
+    if (req.filter.matches( group ) ) {
+      console.log("Group query matched " + group.cn );
+      var group_resp = {}
+      group_resp.dn = req.dn.toString();
+      group_resp.attributes = group;
+      res.send(group_resp);
+      res.end();
+      return next();
     }
   });
 
@@ -37,6 +65,17 @@ server.bind(SUFFIX, function(req, res, next) {
   return next();
 });
 
-server.listen(1389, function() {
+server.listen(ldap_port, ldap_host, function() {
   console.log('LDAP server listening at %s', server.url);
+  console.log(process.env.STACKATO_SERVICES);
 });
+
+// for Stackato
+if(process.env.PORT){
+  var http_port = parseInt(process.env.PORT);
+  var http = require('http');
+  http.createServer(function (req, res) {
+    res.writeHead(200);
+    res.end('OK');
+  }).listen(http_port);
+}
